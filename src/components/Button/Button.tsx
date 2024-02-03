7e08cd5bc1223391be7059e3393aa27818b23049
v.koriagin <v.koriagin@platformaofd.ru>
2024-02-03 16:35:53 +0300

    sb

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./button.css"
+
+export interface ButtonProps{
+    label: string;
+}
+
+const Button = ({label}: ButtonProps) => {
+    return <button className="btn">{label}</button>
+}
+
+export default Button;
